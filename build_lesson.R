8c1361f65c5f812aec8115b57a709235d61dc4b5
François Michonneau <francois.michonneau@gmail.com>
2018-06-14 09:15:14 -0400

    let github take care of rendering the site

--- build_lesson.R
+++ build_lesson.R
@@ -3,9 +3,11 @@ BuildLesson <- R6::R6Class(
                        "BuildLesson", inherit = TicStep,
                        public = list(
                            run = function() {
-                               build_status <- system("make site")
+                               build_status <- system("make lesson-md")
                                if (build_status > 0)
                                    stop("Error during building process")
+                               system("mkdir  _rendered")
+                               system("cp -r \`ls -A | grep -v '.git' | grep -v '_rendered' | grep -v '_site'\` _rendered")
                            })
                    )
 
